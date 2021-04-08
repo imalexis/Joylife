9ac27004b537524b4ade7bc73a583b321c1689fd
imalexis <alexis9007s@gmail.com>
2021-04-08 19:25:10 +0800

    Initial Commit

---
+++ Joylife/JoylifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoylifeApp.swift
+//  Joylife
+//
+//  Created by alexis on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct JoylifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
